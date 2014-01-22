d3df005cebe32384ae33a09eb83e169a6b92388e
optimusmoose <2robsmith@gmail.com>
2014-01-22 09:14:31 -0700

    License header

--- src/simulatorGUI/simulatorGUI.java
+++ src/simulatorGUI/simulatorGUI.java
@@ -1,13 +1,20 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+		JAMSS - MS simulator
+    Copyright (C) 2014  Rob Smith 2robsmith@gmail.com
 
-/*
- * simulatorGUI.java
- *
- * Created on Oct 14, 2013, 2:22:16 PM
- */
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package simulatorGUI;
 
 import java.io.File;
